c7728203b21cfe533ab9023ee48b06457625ad02
cole-campbell <cole.william.campbell@gmail.com>
2016-02-26 15:37:28 +0000

    Login Functionallity available, cannot go to secrets page unless valid password is used.

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,49 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'homepage' });
+});
+
+router.get('/index', function(req, res, next) {
+  res.render('index', { title: 'homepage' });
+});
+
+/* GET home page. */
+router.get('/secrets', function(req, res, next) {
+  res.redirect('/index');
 });
 
 /* GET login page */
-router.get('/login', function(req,res,next){
-   res.render('login', {title: 'Login'}); 
+router.get('/badLogin', function(req,res,next){
+   res.render('badLogin', {title: 'badLogin'}); 
 });
 
+/*  */
+
+// If we get a POST request for /name
+router.post('/index', function(req, res, next) {
+  // SELECT password from DB where username='req.body.yourName'
+    
+  if (req.body.passWord == "password") {
+    res.render('secrets');
+  }
+  else {
+      res.redirect('/badLogin');
+  }
+});
+
+router.post('/logout', function(req, res, next) {
+    res.render('index');
+});
+
+router.post('/badLogin', function(req, res, next) {
+  // SELECT password from DB where username='req.body.yourName'
+    
+  if (req.body.passWord == "password") {
+    res.render('secrets');
+  }
+  else {
+      res.redirect('/badLogin');
+  }
+});
 module.exports = router;
