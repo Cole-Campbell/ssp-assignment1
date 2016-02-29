4a5e8f3f5e17bd8f171ca927ecdea65aeb63a7e6
cole-campbell <cole.william.campbell@gmail.com>
2016-02-29 11:57:51 +0000

    Reading JSON file into index.js file. Now to pull data from the file and display it.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var fs = require('fs');
+var obj;
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page with either / or index. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'homepage' });
 });
@@ -10,42 +12,49 @@ router.get('/index', function(req, res, next) {
   res.render('index', { title: 'homepage' });
 });
 
-/* GET home page. */
-router.get('/secrets', function(req, res, next) {
-  res.redirect('/index');
-});
 
-/* GET login page */
+/* GET bad login page if login is not proper */
 router.get('/badLogin', function(req,res,next){
    res.render('badLogin', {title: 'badLogin'}); 
 });
 
-/*  */
 
-// If we get a POST request for /name
+/* Secrets redirect if user tries to visit Secrets without logging in */
+router.get('/secrets', function(req, res, next) {
+  res.redirect('/index');
+});
+
+
+// Post request for index if password and username are submitted.
 router.post('/index', function(req, res, next) {
-  // SELECT password from DB where username='req.body.yourName'
     
-  if (req.body.userName == "Cole" && req.body.passWord == "password") {
-    res.render('secrets', {name: req.body.userName});
-  }
-  else {
+    if (req.body.userName == "Cole" && req.body.passWord == "password") {
+        res.render('secrets', {name: req.body.userName});
+    }else {
       res.redirect('/badLogin');
-  }
+    }
+    
+    //Read secrets in
+   fs.readFile('secrets.json', 'utf8', (err, data) => {
+       if(err) throw err;
+       console.log(data);
+   });
 });
 
+// Post request for badLogin page if password and username are submitted
 router.post('/badLogin', function(req, res, next) {
-  // SELECT password from DB where username='req.body.yourName'
     
-  if (req.body.userName == "Cole" && req.body.passWord == "password") {
-    res.redirect('secrets', {name: req.body.userName});
-  }
-  else {
-      res.redirect('/badLogin');
-  }
+    if (req.body.userName == "Cole" && req.body.passWord == "password") {
+        res.redirect('secrets', {name: req.body.userName});
+    }else {
+        res.redirect('/badLogin');
+    }
 });
 
+
+//Post for logout button on secrets page.
 router.post('/logout', function(req, res, next) {
     res.render('index');
 });
+
 module.exports = router;
