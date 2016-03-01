4076c2aaa786cdec0e429c2509b807570818bbd4
cole-campbell <cole.william.campbell@gmail.com>
2016-03-01 17:19:10 +0000

    Delete button works now, but deletes all secrets

--- routes/index.js
+++ routes/index.js
@@ -107,7 +107,7 @@ router.post('/remove', function(req,res) {
     
     //Submit information to DB
     collection.remove({
-        "_id": secretid,
+        "id": secretid,
     }, function (err, doc) {
         if(err){
             //If it fails to add to the database then it will return the error
